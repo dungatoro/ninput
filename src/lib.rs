08d905ef7375571b09992da943de79e7cab963a5
dungatoro <dungatoro@arch.localdomain>
2023-09-30 10:08:48 +0100

    add files

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use std::io::Write;
+
+pub fn ninput(message: &str) -> String {
+    let mut input: String;
+    print!("\n{message}");
+    std::io::stdout().flush().expect("Failed to flush stdout");
+    input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.pop();
+    input
+}
