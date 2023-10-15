e97aa33349d2baf1795a94422842e06f9c1ff4e1
dungatoro <dungatoro@arch.localdomain>
2023-10-15 19:55:48 +0100

    chagne

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 
 pub fn ninput(message: &str) -> String {
     let mut input: String;
-    println!("{message}");
+    print!("{message}");
     std::io::stdout().flush().expect("Failed to flush stdout");
     input = String::new();
     std::io::stdin().read_line(&mut input).expect("Failed to read line");
