ac50a615ec56e6d1fa8cc0344c9db02ee65fafd6
dungatoro <dungatoro@arch.localdomain>
2023-10-15 19:53:41 +0100

    change line

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 
 pub fn ninput(message: &str) -> String {
     let mut input: String;
-    print!("\n{message}");
+    println!("{message}");
     std::io::stdout().flush().expect("Failed to flush stdout");
     input = String::new();
     std::io::stdin().read_line(&mut input).expect("Failed to read line");
